ba0ebe61fffd1c36d60a268d727d75cc50c773d7
agone <alex@agon-e.com>
2022-10-12 15:11:40 +0200

    single usage demo, npm update, more options, code style, cart button, basic translations

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
       showRealtimeTable: realtime,
       showDepartures: departures,
       showArrivals: arrivals,
+      showMap: map,
+      theme: theme,
+      showHeader: header,
+      showSidebar: sidebar,
       regionSwitcher: regionswitch,
       timezoneSwitcher: timezoneswitch,
       metricUnits: metricunits,
@@ -15,70 +19,87 @@
       timezones: JSON.parse(timezones),
       maxforecast: maxforecast,
       wss_endpoint: wssendpoint,
-      rest_endpoint: restendpoint,
+      rest_endpoint: restendpoint
     }"
   />
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from "vue"
 
-import NoiFlightControl from "./components/NoiFlightControl.vue";
-import VueLayers from "vuelayers";
+import NoiFlightControl from "./components/NoiFlightControl.vue"
+import VueLayers from "vuelayers"
 
-Vue.use(VueLayers);
+Vue.use(VueLayers)
 
 export default {
   name: "App",
   props: {
     lang: {
       type: String,
-      default: "en",
+      default: "en"
     },
     realtime: {
       type: Boolean,
-      default: true,
+      default: true
     },
     departures: {
       type: Boolean,
-      default: true,
+      default: true
     },
     arrivals: {
       type: Boolean,
-      default: true,
+      default: true
     },
     regionswitch: {
       type: Boolean,
-      default: true,
+      default: true
+    },
+    sidebar: {
+      type: Boolean,
+      default: true
     },
     timezoneswitch: {
       type: Boolean,
-      default: false,
+      default: false
     },
     metricunits: {
       type: Boolean,
-      default: false,
+      default: false
+    },
+    map: {
+      type: Boolean,
+      default: true
+    },
+    theme: {
+      type: String,
+      // "terminal" || "skyalps"
+      default: "terminal"
+    },
+    header: {
+      type: Boolean,
+      default: true
     },
     timezone: {
       type: String,
-      default: "local",
+      default: "local"
       // https://www.npmjs.com/package/tzdata list
     },
     wssendpoint: {
       type: String,
-      default: "wss://ws.datapool.opendatahub.testingmachine.eu/flightdata/sbs-aggregated",
+      default: "wss://ws.datapool.opendatahub.testingmachine.eu/flightdata/sbs-aggregated"
     },
     restendpoint: {
       type: String,
-      default: "https://api.datapool.opendatahub.testingmachine.eu/flightdata-scheduled",
+      default: "https://api.datapool.opendatahub.testingmachine.eu/flightdata-scheduled"
     },
     maxage: {
       type: Number,
-      default: 60,
+      default: 60
     },
     maxforecast: {
       type: Number,
-      default: 100,
+      default: 100
     },
     colors: {
       type: String,
@@ -87,9 +108,9 @@ export default {
           primary: "#0068B4",
           secondary: "#555555",
           primary_contrast: "#ffffff",
-          secondary_contrast: "#ffffff",
-        });
-      },
+          secondary_contrast: "#ffffff"
+        })
+      }
     },
     regions: {
       type: String,
@@ -98,34 +119,34 @@ export default {
           {
             label: "Airport Bolzano/Bozen",
             center: [11.3361503, 46.4607403],
-            zoom: 14.25,
+            zoom: 14.25
           },
           {
             label: "Region",
             center: [11.463606010174134, 46.42665096950925],
-            zoom: 10,
-          },
-        ]);
-      },
+            zoom: 10
+          }
+        ])
+      }
     },
     timezones: {
       type: String,
       default: () => {
         return JSON.stringify([
           {
-            label: 'UTC',
-            code: 'UTC'
+            label: "UTC",
+            code: "UTC"
           },
           {
-            label: 'Local time',
-            code: 'local'
-          },
-        ]);
-      },
-    },
+            label: "Local time",
+            code: "local"
+          }
+        ])
+      }
+    }
   },
   components: {
-    NoiFlightControl,
-  },
-};
+    NoiFlightControl
+  }
+}
 </script>
