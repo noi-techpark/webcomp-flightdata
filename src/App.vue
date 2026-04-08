ef2ba3e4d717c22e85681f382ea4ed71897de48c
IanVincent.Ramos <IRamos@unibz.it>
2026-04-08 22:37:38 +0200

    Integrated the new SkyAlps flights data and also fixed the booking link the booking button now leads you to the booking link directly.

--- src/App.vue
+++ src/App.vue
@@ -141,7 +141,7 @@ export default {
     },
     restendpoint: {
       type: String,
-      default: "https://mobility.api.opendatahub.com/v2/flat%2Cnode/Flight?"
+      default: "https://tourism.api.opendatahub.com/v1/Trip?"
     },
     maxage: {
       type: Number,
